af8f3a78169dcc25d38c93fe9e071d99a43ad944
Bishoy Boules <79402748+BishoyBoules@users.noreply.github.com>
2023-04-09 18:59:40 +0200

    Updates

---
+++ precache-manifest.eb3310e74ec1f39a88adffc733c2485c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e2bb066860d8f01a630",
+    "url": "/React-Yeshtery/static/css/main.53afd3a9.chunk.css"
+  },
+  {
+    "revision": "6e2bb066860d8f01a630",
+    "url": "/React-Yeshtery/static/js/main.6e2bb066.chunk.js"
+  },
+  {
+    "revision": "2dc07302c40e4c50d7cf",
+    "url": "/React-Yeshtery/static/css/1.a7a4546f.chunk.css"
+  },
+  {
+    "revision": "2dc07302c40e4c50d7cf",
+    "url": "/React-Yeshtery/static/js/1.2dc07302.chunk.js"
+  },
+  {
+    "revision": "94ee95a9202d68e4ea32",
+    "url": "/React-Yeshtery/static/js/2.94ee95a9.chunk.js"
+  },
+  {
+    "revision": "a3950e57804d8aef6d93",
+    "url": "/React-Yeshtery/static/js/3.a3950e57.chunk.js"
+  },
+  {
+    "revision": "ac951601ea2a4ffd772b",
+    "url": "/React-Yeshtery/static/js/runtime~main.ac951601.js"
+  },
+  {
+    "revision": "a137d91bc0b89a047e5fef444deae618",
+    "url": "/React-Yeshtery/static/media/second.a137d91b.png"
+  },
+  {
+    "revision": "3afb604ae4d8072697478a72f3b6a20a",
+    "url": "/React-Yeshtery/static/media/first.3afb604a.png"
+  },
+  {
+    "revision": "63d210b07b8e7fb6dd80bdd2f1c2bcde",
+    "url": "/React-Yeshtery/static/media/shirt.63d210b0.png"
+  },
+  {
+    "revision": "984085501ec2df271e41401d04fd10f0",
+    "url": "/React-Yeshtery/static/media/twitter-logo.98408550.png"
+  },
+  {
+    "revision": "82968cfe051540fcf2251d7ce1291d02",
+    "url": "/React-Yeshtery/index.html"
+  }
+];
